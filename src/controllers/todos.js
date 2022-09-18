773aac243a8924365f0d9a6a573f7d5fdf59f207
Alex <alexandre@predictalab.com>
2022-09-18 07:48:36 +0200

    feat/get one todo

--- src/controllers/todos.js
+++ src/controllers/todos.js
@@ -1,6 +1,6 @@
 import Todo from '../models/todo.js'
 
-// Get all todos
+// get all todos
 export const getAllTodos = async (req, res) => {
   try {
     const todos = await Todo.find({}).exec()
@@ -11,13 +11,30 @@ export const getAllTodos = async (req, res) => {
   }
 }
 
+// create one todo
 export const createOneTodo = async (req, res) => {
   try {
-    console.log(req)
     const todo = await Todo.create({ ...req.body })
     res.status(201).json({ data: todo })
   } catch (error) {
     console.error(error)
     res.status(400).end()
+  }
+}
+
+// get one todo
+export const getOneTodo = async (req, res) => {
+  try {
+    const oneTodo = await Todo.findOne({ _id: req.params.id }).lean().exec()
+
+    if (!oneTodo)
+      return res.status(400).json({ detail: 'No todo found with this ID' })
+
+    res.status(200).json({ data: oneTodo })
+  } catch (error) {
+    console.error(error)
+    res
+      .status(400)
+      .json({ detail: 'Error while retrieving a todo with this ID' })
   }
 }
--- src/routes/todos.js
+++ src/routes/todos.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllTodos, createOneTodo } from '../controllers/todos.js'
+import { getAllTodos, createOneTodo, getOneTodo } from '../controllers/todos.js'
 
 const router = express.Router()
 
@@ -7,6 +7,6 @@ const router = express.Router()
 router.route('/').get(getAllTodos).post(createOneTodo)
 
 // get, update or delete one todo
-router.route('/:id').get().put().delete()
+router.route('/:id').get(getOneTodo).put().delete()
 
 export default router
